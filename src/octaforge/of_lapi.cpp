bbaa5fec79293d0b6dddf12146b76d77cbbd8dcd
q66 <quaker66@gmail.com>
2013-04-10 19:49:33 +0200

    do not expose the OFTL lua state anymore and remove the silly lua utils

--- src/octaforge/of_lapi.cpp
+++ src/octaforge/of_lapi.cpp
@@ -310,37 +310,3 @@ namespace lapi
         return true;
     }
 } /* end namespace lapi */
-
-namespace lua {
-    void push(lua_State *L, bool v) {
-        lua_pushboolean(L, v);
-    }
-
-    void push(lua_State *L, lua_CFunction v) {
-        lua_pushcfunction(L, v);
-    }
-
-    void push(lua_State *L, const char *v) {
-        lua_pushstring(L, v);
-    }
-
-    void push(lua_State *L, lua_Number v) {
-        lua_pushnumber(L, v);
-    }
-
-    void push(lua_State *L, lua_Integer n) {
-        lua_pushinteger(L, n);
-    }
-
-    void push(lua_State *L, lua_CFunction f, int n) {
-        lua_pushcclosure(L, f, n);
-    }
-
-    void push(lua_State *L, const char *s, size_t l) {
-        lua_pushlstring(L, s, l);
-    }
-
-    void push(lua_State *L) {
-        lua_pushnil(L);
-    }
-}
--- src/octaforge/of_lapi.h
+++ src/octaforge/of_lapi.h
@@ -5,23 +5,10 @@
 
 namespace lapi
 {
-    extern lua::State state;
     extern lua_State *L;
-
     void init        (const char *dir = "data/library/core");
     void reset       ();
     bool load_library(const char *name);
-}
-
-namespace lua {
-    void push(lua_State *L, bool v);
-    void push(lua_State *L, lua_CFunction v);
-    void push(lua_State *L, const char *v);
-    void push(lua_State *L, lua_Number v);
-    void push(lua_State *L, lua_Integer n);
-    void push(lua_State *L, lua_CFunction f, int n);
-    void push(lua_State *L, const char *s, size_t l);
-    void push(lua_State *L);
 }
 
 #endif
