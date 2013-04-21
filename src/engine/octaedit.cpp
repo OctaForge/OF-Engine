2b280ac10357e4f1763741efe20641999e27ac9d
q66 <quaker66@gmail.com>
2013-04-21 16:34:25 +0200

    fixed slot drawing

--- src/engine/octaedit.cpp
+++ src/engine/octaedit.cpp
@@ -2315,7 +2315,7 @@ void drawslot(Slot &slot, VSlot &vslot, float w, float h, float sx, float sy) {
     VSlot *layer = NULL;
     if (slot.loaded) {
         tex = slot.sts[0].t;
-        if(slot.texmask&(1<<TEX_GLOW)) {
+        if (slot.texmask&(1<<TEX_GLOW)) {
             loopv(slot.sts) if(slot.sts[i].type==TEX_GLOW)
             { glowtex = slot.sts[i].t; break; }
         }
@@ -2325,32 +2325,32 @@ void drawslot(Slot &slot, VSlot &vslot, float w, float h, float sx, float sy) {
                 layertex = layer->slot->sts[0].t;
         }
     }
-    else if (slot.thumbnail) tex = slot.thumbnail;
+    else if(slot.thumbnail) tex = slot.thumbnail;
     float xt, yt;
     xt = min(1.0f, tex->xs/(float)tex->ys),
     yt = min(1.0f, tex->ys/(float)tex->xs);
 
-    static Shader *rgbonlyshader = NULL;
-    if (!rgbonlyshader) rgbonlyshader = lookupshaderbyname("rgbonly");
-    rgbonlyshader->set();
-
-    vec2 tc[4] = { vec2(0, 0), vec2(1, 0), vec2(1, 1), vec2(0, 1) };
+    vec2 tc[4] = { vec2(0, 0), vec2(1, 0), vec2(0, 1), vec2(1, 1) };
     int xoff = vslot.offset.x, yoff = vslot.offset.y;
     if (vslot.rotation) {
-        if ((vslot.rotation&5) == 1) { swap(xoff, yoff); loopk(4) swap(tc[k].x, tc[k].y); }
-        if (vslot.rotation >= 2 && vslot.rotation <= 4) { xoff *= -1; loopk(4) tc[k].x *= -1; }
-        if (vslot.rotation <= 2 || vslot.rotation == 5) { yoff *= -1; loopk(4) tc[k].y *= -1; }
+        if ((vslot.rotation&5) == 1) { swap(xoff, yoff); loopk(4) swap(tc[k][0], tc[k][1]); }
+        if (vslot.rotation >= 2 && vslot.rotation <= 4) { xoff *= -1; loopk(4) tc[k][0] *= -1; }
+        if (vslot.rotation <= 2 || vslot.rotation == 5) { yoff *= -1; loopk(4) tc[k][1] *= -1; }
     }
-    loopk(4) { tc[k].x = tc[k].x/xt - float(xoff)/tex->xs; tc[k].y = tc[k].y/yt - float(yoff)/tex->ys; }
-    gle::color(slot.loaded ? vslot.colorscale : vec(1, 1, 1));
+    loopk(4) { tc[k][0] = tc[k][0]/xt - float(xoff)/tex->xs; tc[k][1] = tc[k][1]/yt - float(yoff)/tex->ys; }
+
+    if (slot.loaded) SETSHADER(hudrgb);
+    else hudshader->setvariant(hasTRG ? (tex->bpp==1 ? 0 : (tex->bpp==2 ? 1 : -1)) : -1, 0);
+
     glBindTexture(GL_TEXTURE_2D, tex->id);
-    gle::defvertex(2);
-    gle::deftexcoord0();
+
+    if (slot.loaded) gle::color(vslot.colorscale);
+    else gle::colorf(1, 1, 1);
     gle::begin(GL_TRIANGLE_STRIP);
-    gle::attribf(sx,     sy);     gle::attrib(tc[0]);
-    gle::attribf(sx + w, sy);     gle::attrib(tc[1]);
-    gle::attribf(sx,     sy + h); gle::attrib(tc[3]);
-    gle::attribf(sx + w, sy + h); gle::attrib(tc[2]);
+    gle::attribf(sx  , sy  ); gle::attrib(tc[0]);
+    gle::attribf(sx+w, sy  ); gle::attrib(tc[1]);
+    gle::attribf(sx  , sy+h); gle::attrib(tc[2]);
+    gle::attribf(sx+w, sy+h); gle::attrib(tc[3]);
     gle::end();
 
     if (glowtex) {
@@ -2358,26 +2358,25 @@ void drawslot(Slot &slot, VSlot &vslot, float w, float h, float sx, float sy) {
         glBindTexture(GL_TEXTURE_2D, glowtex->id);
         gle::color(vslot.glowcolor);
         gle::begin(GL_TRIANGLE_STRIP);
-        gle::attribf(sx,     sy);     gle::attrib(tc[0]);
-        gle::attribf(sx + w, sy);     gle::attrib(tc[1]);
-        gle::attribf(sx,     sy + h); gle::attrib(tc[3]);
-        gle::attribf(sx + w, sy + h); gle::attrib(tc[2]);
+        gle::attribf(sx  , sy  ); gle::attrib(tc[0]);
+        gle::attribf(sx+w, sy  ); gle::attrib(tc[1]);
+        gle::attribf(sx  , sy+h); gle::attrib(tc[2]);
+        gle::attribf(sx+w, sy+h); gle::attrib(tc[3]);
         gle::end();
+
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     }
     if (layertex) {
         glBindTexture(GL_TEXTURE_2D, layertex->id);
         gle::color(layer->colorscale);
         gle::begin(GL_TRIANGLE_STRIP);
-        gle::attribf(sx + w / 2, sy + h / 2); gle::attrib(tc[0]);
-        gle::attribf(sx + w,     sy + h / 2); gle::attrib(tc[1]);
-        gle::attribf(sx + w / 2, sy + h);     gle::attrib(tc[3]);
-        gle::attribf(sx + w,     sy + h);     gle::attrib(tc[2]);
+        gle::attribf(sx  , sy  ); gle::attrib(tc[0]);
+        gle::attribf(sx+w, sy  ); gle::attrib(tc[1]);
+        gle::attribf(sx  , sy+h); gle::attrib(tc[2]);
+        gle::attribf(sx+w, sy+h); gle::attrib(tc[3]);
         gle::end();
     }
-
-    gle::color(vec(1, 1, 1));
-    hudshader->set();
+    gle::colorf(1, 1, 1);
 }
 
 LUAICOMMAND(texture_draw_slot, {
