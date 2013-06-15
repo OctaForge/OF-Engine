29c700245ede749231877446aca51fd25f5a3df2
q66 <quaker66@gmail.com>
2013-06-15 20:58:13 +0200

    cut away some silly testing gamemanager code

--- media/map/empty/map.lua
+++ media/map/empty/map.lua
@@ -24,32 +24,10 @@ require("10.mapscripts.drawing")
 
 -- this is how you initialize game manager
 game_manager.setup({
-    game_manager.manager_plugins.messages,
-    game_manager.manager_plugins.event_list,
     projectiles.plugin,
     extraevents.actions_parallel_plugin
 })
-get_scoreboard_text = game_manager.get_scoreboard_text
-
---[[
--- enable for bot player
-entity_classes.register(plugins.bake(
-    character.player, {
-        health.plugin,
-        {
-            init = function(self)
-                self:set_attr("model_name", "player")
-            end
-        }
-    }, "bot_player"
-), "fpsent")
-]]
 
 -- this function will run on server only (condition inside it)
 -- it loads the entities into server storage and sends to clients
 ents.load()
-
--- enable for bot player
---if SERVER then
---    edit.add_npc("bot_player")
---end
