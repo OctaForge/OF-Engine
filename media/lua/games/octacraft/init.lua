a38e9276b4ea523fc5b6acd27cf430872013bb46
q66 <quaker66@gmail.com>
2013-10-13 14:43:17 +0200

    action for block placement/deletion (allows for repeating placement/deletion)

--- media/lua/games/octacraft/init.lua
+++ media/lua/games/octacraft/init.lua
@@ -18,6 +18,7 @@ local log = require("core.logger")
 
 local input = require("core.engine.input")
 local inputev = require("core.events.input")
+local actions = require("core.events.actions")
 local edit = require("core.engine.edit")
 local cs = require("core.engine.cubescript")
 local signal = require("core.events.signal")
@@ -44,30 +45,66 @@ ents.register_class(Game_Player, {
 ents.register_class(ents.Obstacle, { health.deadly_area_plugin },
     "Deadly_Area")
 
-local cube_size = 3
+cs.var_set("player_class", "Game_Player")
 
-if not SERVER then
-    inputev.set_event("click", function(btn, down, x, y, z, ent, cx, cy)
-        if ent and ent.click then
-            return ent:click(btn, down, x, y, z, cx, cy)
+if SERVER then return end
+
+local Mouse_Action = actions.Action:clone {
+    name = "Mouse_Action",
+    allow_multiple = false,
+    block_size = 3,
+
+    __start = function(self)
+        self.counter = 0
+        self:try_block()
+    end,
+
+    __run = function(self, millis)
+        local cnt = self.counter
+        cnt += millis
+        local btn = self.button
+        if (btn == 1 and cnt >= 300) or cnt >= 100 then
+            self.counter = 0
+            self:try_block()
+        else
+            self.counter = cnt
         end
-        if not down then return end
-        local pl = ents.get_player()
-        local tg = geom.Vec3(x, y, z)
+        return false
+    end,
+
+    try_block = function(self)
+        local pl = self.player
+        local tg = input.get_target_position()
         local pos = pl:get_attr("position"):copy()
         pos.z += pl:get_attr("eye_height")
         local bf
-        if btn == 1 then
+        if self.button == 1 then
             tg:add((tg - pos):normalize())
             bf = edit.delete_cube
         else
             tg:sub((tg - pos):normalize())
             bf = edit.create_cube
         end
-        bf(tg.x >> cube_size << cube_size,
-           tg.y >> cube_size << cube_size,
-           tg.z >> cube_size << cube_size, 1 << cube_size)
-    end)
-end
+        local bsize = self.block_size
+        bf(tg.x >> bsize << bsize, tg.y >> bsize << bsize,
+           tg.z >> bsize << bsize, 1 << bsize)
+    end
+}
 
-cs.var_set("player_class", "Game_Player")
+inputev.set_event("click", function(btn, down, x, y, z, ent, cx, cy)
+    if ent and ent.click then
+        return ent:click(btn, down, x, y, z, cx, cy)
+    end
+    local gm = game_manager.get()
+    if down then
+        local pl = ents.get_player()
+        local mact = Mouse_Action()
+        mact.button = btn
+        mact.player = pl
+        gm.mouse_action = mact
+        gm:queue_action(mact)
+    else
+        gm.mouse_action:cancel()
+        gm.mouse_action = nil
+    end
+end)
