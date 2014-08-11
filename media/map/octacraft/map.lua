2ed52872092810206079fe5255746d5957d01e34
q66 <quaker66@gmail.com>
2014-08-11 21:43:53 +0100

    add a map with octacraft enabled by default (currently empty)

---
+++ media/map/octacraft/map.lua
@@ -0,0 +1,20 @@
+-- Octacraft testmap
+-- author: q66 <quaker66@gmail.com>
+
+local ents = require("core.entities.ents")
+
+local game_manager = require("extra.game_manager")
+
+require("extra.entities.teleporters")
+require("extra.entities.lights")
+require("extra.entities.particles")
+
+-- drawing game
+require("games.octacraft")
+
+-- this is how you initialize game manager
+game_manager.setup()
+
+-- this function will run on server only (condition inside it)
+-- it loads the entities into server storage and sends to clients
+ents.load()
