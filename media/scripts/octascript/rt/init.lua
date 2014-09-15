a328f7675a457aef06b3ef8d7a105915fb4a838c
q66 <quaker66@gmail.com>
2014-09-15 19:50:57 +0100

    octascript: tostring(undef) now returns "undef" rather than "nil"

--- media/scripts/octascript/rt/init.lua
+++ media/scripts/octascript/rt/init.lua
@@ -33,11 +33,16 @@ M.null = ffi.cast("void*", 0)
 local mt = debug.getmetatable(M.null)
 
 local prev_eq = mt.__eq
+
 mt.__eq = function(self, o)
     if type(o) ~= "cdata" then
         return false
     end
     return prev_eq(self, o)
 end
+
+debug.setmetatable(nil, {
+    __tostring = function() return "undef" end
+})
 
 return M
