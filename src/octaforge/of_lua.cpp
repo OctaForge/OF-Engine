OctaForge/OF-Engine

commit a7187572fe7399fc8b32cb2762f5f4b2de0b1854

better externals handling

src/octaforge/of_lua.cpp
@@ -28,13 +28,12 @@ namespace lua
 
     static void setup_binds(bool dedicated);
 
-    static int external_handler = LUA_REFNIL;
+    static int externals = LUA_REFNIL;
 
     static bool push_external(const char *name) {
-        if (external_handler == LUA_REFNIL) return false;
-        lua_rawgeti(L, LUA_REGISTRYINDEX, external_handler);
-        lua_pushstring(L, name);
-        lua_call(L, 1, 1);
+        if (externals == LUA_REFNIL) return false;
+        lua_rawgeti(L, LUA_REGISTRYINDEX, externals);
+        lua_getfield(L, -1, name);
         if (lua_isnil(L, -1)) {
             lua_pop(L, 1);
             return false;
@@ -193,7 +192,7 @@ namespace lua
 
     LUAICOMMAND(external_hook, {
         lua_pushvalue(L, 1);
-        external_handler = luaL_ref(L, LUA_REGISTRYINDEX);
+        externals = luaL_ref(L, LUA_REGISTRYINDEX);
         return 0;
     })
 

media/scripts/core/externals.oct
@@ -37,7 +37,7 @@ M.set = func(name, fun) {
     return old
 }
 
-capi::external_hook(M.get)
+capi::external_hook(externals)
 
 // we need the state restore external here so that it gets cleaned up with
 // the rest

commit 68bdf181aab55a9ae9e1e3b2cc579404964bf590

fix a stack overflow bug introduced by a718757

src/octaforge/of_lua.cpp
@@ -35,9 +35,10 @@ namespace lua
         lua_rawgeti(L, LUA_REGISTRYINDEX, externals);
         lua_getfield(L, -1, name);
         if (lua_isnil(L, -1)) {
-            lua_pop(L, 1);
+            lua_pop(L, 2);
             return false;
         }
+        lua_replace(L, -2);
         return true;
     }
 